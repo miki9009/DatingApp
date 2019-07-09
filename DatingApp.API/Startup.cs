d62525aae85a8912a39f7ccfb7a341567b7033de
mikolaj <mikolaj.rambiert@gmail.com>
2019-07-09 19:35:07 +0200

    User controller

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -35,7 +35,11 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(o=>
+                {
+                    o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
 
             services.AddScoped<IAuthRepository, AuthRepository>();
