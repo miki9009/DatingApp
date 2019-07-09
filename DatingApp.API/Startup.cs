7c3834d002c70aa6c980ef3cf9a6db780f56f7f1
mikolaj <mikolaj.rambiert@gmail.com>
2019-07-09 18:57:34 +0200

    Database update

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -48,10 +48,12 @@ namespace DatingApp.API
                     ValidateAudience = false
                 };
             });
+
+            services.AddTransient<Seed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -82,9 +84,11 @@ namespace DatingApp.API
             
 
             // app.UseHttpsRedirection();
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
+
         }
     }
 }
